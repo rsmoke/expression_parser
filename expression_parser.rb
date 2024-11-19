539e56e8aded20b8b4c0c7df0e611da2a2264370
rsmokeUM <rsmoke@umich.edu>
2024-11-19 17:05:55 -0500

    Add variable? method and make recursive

--- expression_parser.rb
+++ expression_parser.rb
@@ -2,15 +2,15 @@ require_relative 'expression_tree'
 
 class ExpressionParser
   def self.parse(expression)
-    if constant?(expression)
+    if constant?(expression) || variable?(expression)
       return ExpressionTree.new(root: expression,
                                 left_child: nil,
                                 right_child: nil)
     end
     if expression.include?('+')
       return ExpressionTree.new(root: '+',
-                                left_child: expression.split('+')[0],
-                                right_child: expression.split('+')[1])
+                                left_child: ExpressionParser.parse(expression.split('+')[0]),
+                                right_child: ExpressionParser.parse(expression.split('+')[1]))
     end
     operators = ['/', '*', '-', '^']
     if operators.any? { |op| expression.include?(op) }
@@ -25,5 +25,9 @@ class ExpressionParser
 
   def self.constant?(expression)
     expression.match?(/^\d+$/)
+  end
+
+  def self.variable?(expression)
+    expression.match?(/^[A-Za-z]+$/)
   end
 end
