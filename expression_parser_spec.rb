a66f6d0e2e00a0eb4d396fa9f78f2340c738f998
rsmokeUM <rsmoke@umich.edu>
2024-11-19 17:05:02 -0500

    expand the expect to include root

--- expression_parser_spec.rb
+++ expression_parser_spec.rb
@@ -37,11 +37,11 @@ RSpec.describe ExpressionParser do
     end
     it 'returns with left child 2' do
       expression = ExpressionParser.parse('2+X')
-      expect(expression.left_child).to eq('2')
+      expect(expression.left_child.root).to eq('2')
     end
     it 'returns with right child X' do
       expression = ExpressionParser.parse('2+X')
-      expect(expression.right_child).to eq('X')
+      expect(expression.right_child.root).to eq('X')
     end
   end
   context 'the expression is a constant' do
