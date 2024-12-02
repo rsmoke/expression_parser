7d3bee851661ded506f1d1dcb5ac1c04465642b5
rsmokeUM <rsmoke@umich.edu>
2024-12-02 11:24:37 -0500

    Refactor expression parsing to improve operator handling
    - Introduced methods `whats_to_the_left_of_the` and `whats_to_the_right_of_the` for better clarity and separation of concerns in parsing expressions.
    - Updated the logic in the `parse` method to utilize these new methods for handling the '+' operator, enhancing readability and maintainability.

--- expression_parser.rb
+++ expression_parser.rb
@@ -7,11 +7,16 @@ class ExpressionParser
                                 left_child: nil,
                                 right_child: nil)
     end
+    # 4x + 2y
+    # left_child: whats to the left of the +
+    # right_child: whats to the right of the +
+
     if expression.include?('+')
       return ExpressionTree.new(root: '+',
-                                left_child: ExpressionParser.parse(expression.split('+')[0]),
-                                right_child: ExpressionParser.parse(expression.split('+')[1]))
+                                left_child: whats_to_the_left_of_the(expression, '+'),
+                                right_child: whats_to_the_right_of_the(expression, '+'))
     end
+
     operators = ['/', '*', '-', '^']
     if operators.any? { |op| expression.include?(op) }
       return ExpressionTree.new(root: expression[1],
@@ -29,5 +34,17 @@ class ExpressionParser
 
   def self.variable?(expression)
     expression.match?(/^[A-Za-z]+$/)
+  end
+
+  def self.whats_to_the_left_of_the(expression, operator)
+    ExpressionTree.new(root: expression.split(operator).first,
+                       left_child: nil,
+                       right_child: nil)
+  end
+
+  def self.whats_to_the_right_of_the(expression, operator)
+    ExpressionTree.new(root: expression.split(operator).last,
+                       left_child: nil,
+                       right_child: nil)
   end
 end
